d78225f6acf70586d097acd6792968a03d3b2501
Conrad Mercer <momercers@gmail.com>
2024-11-05 10:29:35 -0600

    Level 3

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 @onready var timer = $Timer 
-@onready var player = $"../../Player"
-@onready var playerLevel2 = %Player
+var player : CharacterBody2D
+#@onready var player = $"../../Player"
+#@onready var playerLevel2 = %Player
 
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
 
 func _on_body_entered(body):
 	timer.start()
@@ -11,13 +14,17 @@ func _on_body_entered(body):
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
-	if Global.level == 1:
-		player.position.x = Global.placeHolderPositionX
-		player.position.y = Global.placeHolderPositionY
-		
-		
-	elif Global.level == 2:
-		playerLevel2.position.x = Global.placeHolderPositionX
-		playerLevel2.position.y = Global.placeHolderPositionY
-		
+	
+	player.position.x = Global.placeHolderPositionX
+	player.position.y = Global.placeHolderPositionY
+	
+	#if Global.level == 1:
+		#player.position.x = Global.placeHolderPositionX
+		#player.position.y = Global.placeHolderPositionY
+		#
+		#
+	#elif Global.level == 2:
+		#playerLevel2.position.x = Global.placeHolderPositionX
+		#playerLevel2.position.y = Global.placeHolderPositionY
+		#
 	
